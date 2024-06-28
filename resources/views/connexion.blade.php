3b085df03b8fef7277c74f798b0741dd7728b754
unknown <hasinamaherymilanto17052000@gmail.com>
2024-06-28 07:57:55 +0200

    edit connexion

--- resources/views/connexion.blade.php
+++ resources/views/connexion.blade.php
@@ -12,7 +12,7 @@
 
 <body style="height: 100vh" class="d-flex align-items-center justify-content-center">
     <form class="w-25 rounded shadow p-2 form_connexion">
-        <h3 class="text-center">Connexion</h3>
+        <h3 class="text-center">Connex</h3>
         <hr>
         @csrf
         <input type="text" name="email" class="form-control mb-2" id="" placeholder="Votre email">
@@ -20,34 +20,34 @@
         <hr>
         <button type="submit" class="btn btn-primary w-100">Se connecter</button>
     </form>
-    
+
 
     <script src="{{ asset('jquery-3.6.0.min.js') }}"></script>
-<script>
-    $('.form_connexion').on('submit',function(e){
-        e.preventDefault()
-        var data=$(this)[0]
-        var formData=new FormData(data)
-        $.ajax({
-            url:'{{URL::to('Se_connecter')}}',
-            type:'post',
-            data:formData,
-            contentType:false,
-            processData:false,
-            success:(response)=>{
-               
-                if(response.success){
-                    window.location.href="{{URL::to('discution')}}"
-                }else if(response.error){
-                  console.log(response.error)
+    <script>
+        $('.form_connexion').on('submit', function(e) {
+            e.preventDefault()
+            var data = $(this)[0]
+            var formData = new FormData(data)
+            $.ajax({
+                url: '{{ URL::to('Se_connecter') }}',
+                type: 'post',
+                data: formData,
+                contentType: false,
+                processData: false,
+                success: (response) => {
+
+                    if (response.success) {
+                        window.location.href = "{{ URL::to('discution') }}"
+                    } else if (response.error) {
+                        console.log(response.error)
+                    }
+                },
+                error: (error) => {
+                    console.log(error)
                 }
-            },
-            error:(error)=>{
-                console.log(error)
-            }
+            })
         })
-    })
-</script>
+    </script>
 
 </body>
 
